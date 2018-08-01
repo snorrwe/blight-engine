edcf2aa09365f823aa99a5f387718bdece4ba9e6
Daniel Kiss <littlesnorrboy@gmail.com>
2018-08-01 17:12:11 +0200

    Add common tests

--- src/common/aabb.rs
+++ src/common/aabb.rs
@@ -6,10 +6,35 @@ pub struct AABB {
 }
 
 impl AABB {
-    fn new(center: Vector2, width: f32, height: f32) -> AABB {
+    pub fn new(center: Vector2, width: f32, height: f32) -> AABB {
         AABB {
             center: center,
             radius: Vector2::new(width / 2.0, height / 2.0),
         }
+    }
+
+    pub fn intersects(&self, other: &AABB) -> bool {
+        (self.center.x - other.center.x).abs() <= self.radius.x + other.radius.x
+            && (self.center.y - other.center.y).abs() <= self.radius.y + other.radius.y
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_aabb_intersects_itself() {
+        let aabb = AABB::new(Vector2::new(-1., 0.), 2., 1.);
+        assert!(aabb.intersects(&aabb));
+    }
+
+    #[test]
+    fn test_simple_intersection() {
+        let lhs = AABB::new(Vector2::new(-1., 0.), 2., 1.);
+        let rhs = AABB::new(Vector2::new(1., 0.), 2.1, 1.);
+
+        assert!(lhs.intersects(&rhs));
+        assert!(rhs.intersects(&lhs));
     }
 }
--- src/common/vector2.rs
+++ src/common/vector2.rs
@@ -1,3 +1,5 @@
+use std::ops::{Add, Mul, Sub};
+
 pub struct Vector2 {
     pub x: f32,
     pub y: f32,
@@ -6,5 +8,74 @@ pub struct Vector2 {
 impl Vector2 {
     pub fn new(x: f32, y: f32) -> Vector2 {
         Vector2 { x: x, y: y }
+    }
+}
+
+impl Add<Vector2> for Vector2 {
+    type Output = Vector2;
+
+    fn add(self, rhs: Vector2) -> Vector2 {
+        Vector2 {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+        }
+    }
+}
+
+impl Sub<Vector2> for Vector2 {
+    type Output = Vector2;
+
+    fn sub(self, rhs: Vector2) -> Vector2 {
+        Vector2 {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+        }
+    }
+}
+
+impl Mul<Vector2> for f32 {
+    type Output = Vector2;
+
+    fn mul(self, rhs: Vector2) -> Vector2 {
+        Vector2 {
+            x: rhs.x * self,
+            y: rhs.y * self,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_simple_add() {
+        let lhs = Vector2::new(0., 0.);
+        let rhs = Vector2::new(1., 3.);
+
+        let result = lhs + rhs;
+
+        assert_eq!(result.x, 1.0);
+        assert_eq!(result.y, 3.0);
+    }
+
+    #[test]
+    fn test_simple_sub() {
+        let lhs = Vector2::new(0., 0.);
+        let rhs = Vector2::new(1., 3.);
+
+        let result = lhs - rhs;
+
+        assert_eq!(result.x, -1.0);
+        assert_eq!(result.y, -3.0);
+    }
+
+    #[test]
+    fn tesT_simple_scalar_mul() {
+        let vec = Vector2::new(2.0, 3.0);
+        let result = 0.5 * vec;
+
+        assert_eq!(result.x, 1.0);
+        assert_eq!(result.y, 1.5);
     }
 }
